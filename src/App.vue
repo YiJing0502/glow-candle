2ae5dda7b51ef7b93c6ba88cd1be7ccf9fd10522
YiJing0502 <w71080635@gmail.com>
2024-02-02 14:18:00 +0800

    feat: 新增vue-loading-overlay套件＆fix:修改主要SCSS名稱為all.scss

--- src/App.vue
+++ src/App.vue
@@ -21,5 +21,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style lang="scss">
-@import './assets/main.scss';
+@import './assets/all.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -8,8 +8,13 @@ import router from './router'
 // 載入 vue-axios
 import axios from 'axios';
 import VueAxios from 'vue-axios';
+// 載入 vue-loading-overlay
+import {LoadingPlugin} from 'vue-loading-overlay';
+import 'vue-loading-overlay/dist/css/index.css';
 
 const app = createApp(App)
+// 使用 vue-loading-overlay
+app.use(LoadingPlugin);
 
 app.use(createPinia())
 app.use(router)
