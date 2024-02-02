c93de1a48dc19c16d3bc600f7e1e93fd82cf6139
YiJing0502 <w71080635@gmail.com>
2024-02-02 12:06:01 +0800

    feat: 在專案中載入 vue-axios 並測試確保可運作

--- src/main.js
+++ src/main.js
@@ -5,10 +5,14 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+// 載入 vue-axios
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+// 使用 vue-axios
+app.use(VueAxios, axios);
 app.mount('#app')
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    {{  data }}
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<script>
+export default {
+  data() {
+    return {
+      data: {},
+    };
+  },
+  mounted() {
+    this.$http.get('https://randomuser.me/api/').then((res) => {
+      [this.data] = res.data.results;
+    });
+  },
+};
+</script>
