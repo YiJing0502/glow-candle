de3ecac6f9df1f035f2f33083428ca0046cf784e
YiJing0502 <w71080635@gmail.com>
2024-02-05 09:18:48 +0800

    fix:整合頁數元件

--- src/components/PageBtn.vue
+++ src/components/PageBtn.vue
@@ -1,10 +1,15 @@
 <template>
+  <!-- 上一頁 -->
+  <li class="page-item"><button type="button" class="page-link" :class="{disabled: !prevIsEnabled,}" @click="$emit('change-page')">Previous</button></li>
+  <!-- 頁數 -->
   <li class="page-item"><button type="button" class="page-link" :class="{active: currentPage == page}" :data-page="page"  @click="$emit('change-page', page)">{{page}}</button></li>
+  <!-- 下一頁 -->
+  <li class="page-item"><button type="button" class="page-link" :class="{disabled: !nextIsEnabled,}" @click="$emit('change-page')">Next</button></li>
 </template>
 
 <script>
 export default {
-  props: ['page', 'currentPage'],
+  props: ['page', 'currentPage', 'nextIsEnabled', 'prevIsEnabled'],
   emits: ['change-page'],
 }
 </script>
