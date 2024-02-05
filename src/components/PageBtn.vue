8f81821e70a1732967b346b5bfa638fe5a5316da
YiJing0502 <w71080635@gmail.com>
2024-02-05 09:12:09 +0800

    feat:新增頁數元件

---
+++ src/components/PageBtn.vue
@@ -0,0 +1,10 @@
+<template>
+  <li class="page-item"><button type="button" class="page-link" :class="{active: currentPage == page}" :data-page="page"  @click="$emit('change-page', page)">{{page}}</button></li>
+</template>
+
+<script>
+export default {
+  props: ['page', 'currentPage'],
+  emits: ['change-page'],
+}
+</script>
