a3b7b1d7cda4f07226b661abb3792ccd6ce407d4
YiJing0502 <w71080635@gmail.com>
2024-02-05 14:06:35 +0800

    feat:新增優惠券元件、fix:將優惠券管理功能修正為可正常使用

---
+++ src/components/backend/CouponModal.vue
@@ -0,0 +1,102 @@
+<template>
+  <!-- 優惠券modal -->
+  <div class="modal fade" ref="modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true"
+      data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">{{inEditCouponMode ? '編輯優惠券' : '新增優惠券'}}</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+          <div class="row">
+            <div class="col-10">
+              <div class="mb-3">
+                <label for="coupon_title" class="form-label">標題</label>
+                <input type="text" class="form-control" id="coupon_title" placeholder="請輸入標題"
+                  v-model="updatedShowData.title">
+              </div>
+            </div>
+            <div class="col-2 d-flex align-items-center">
+              <div class="form-check mt-3">
+                <input type="checkbox" class="form-check-input" id="is_enabled" :true-value="1" :false-value="0"
+                  v-model="updatedShowData.is_enabled">
+                <label for="is_enabled" class="form-check-label">
+                  <span v-if="updatedShowData.is_enabled === 1" class="text-main-spec fw-bold">啟用</span>
+                  <span v-else class="text-deep-gray">未啟用</span>
+                </label>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mb-3">
+              <label for="coupon_due_date" class="form-label">到期日</label>
+              <input type="date" class="form-control" id="coupon_due_date" placeholder="請輸入到期日"
+                v-model="updatedShowData.due_date" :min="currentDate">
+            </div>
+            <div class="col mb-3">
+              <label for="coupon_percent" class="form-label">折扣</label>
+              <input type="number" class="form-control" id="coupon_percent" placeholder="請輸入折扣"
+                v-model="updatedShowData.percent">
+            </div>
+            <div class="col mb-3">
+              <label for="coupon_code" class="form-label">折扣代碼</label>
+              <input type="text" class="form-control" id="coupon_code" placeholder="請輸入折扣代碼" v-model="updatedShowData.code">
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-normal-dpgray" data-bs-dismiss="modal">關閉</button>
+          <button type="button" class="btn btn-solid-spec"
+            @click="inEditCouponMode ? putAdminCoupon() : postAdminCoupon()">{{inEditCouponMode ? '更新' :
+            '新增'}}</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  // pinia
+  import { mapState } from 'pinia';
+  import timeStore from '@/stores/timeStore';
+  // modal
+  import { Modal } from 'bootstrap';
+  export default {
+    data(){
+      return {
+        modal: '',
+        updatedShowData: {},
+      }
+    },
+    methods:{
+      openModal() {
+      this.modal.show();
+      },
+      hideModal() {
+        this.modal.hide();
+      },
+      putAdminCoupon() {
+        this.$emit('put-admin-coupon', this.updatedShowData);
+      },
+      postAdminCoupon() {
+        this.$emit('post-admin-coupon', this.updatedShowData);
+      },
+    },
+    computed: {
+      ...mapState(timeStore, ['currentDate',]),
+    },
+    watch:{
+      showData(){
+        this.updatedShowData = JSON.parse(JSON.stringify(this.showData));
+      },
+    },
+    props: ['inEditCouponMode', 'showData'],
+    emits: ['put-admin-coupon', 'post-admin-coupon'],
+    mounted() {
+      // 獲取 bsResultModal DOM
+      // 建立 bootstrap modal 實體
+      this.modal = new Modal(this.$refs.modal);
+      this.updatedShowData = JSON.parse(JSON.stringify(this.showData));
+    },
+  }
+</script>
--- src/views/backend/AdminCouponsView.vue
+++ src/views/backend/AdminCouponsView.vue
@@ -1,9 +1,191 @@
 <template>
-  <div >
-    
+  <VueLoading 
+  v-if="!getRemoteData" 
+  :active="!getRemoteData"
+  :background-color="'#FBFAF4'"
+  :color="'#52504B'"/>
+  <div v-else class="container">
+    <div class="row mt-4">
+      <div class="col d-flex justify-content-end">
+        <button type="button" class="btn btn-solid-spec" @click="getAdminAddCouponModal">建立新的優惠券</button>
+      </div>
+    </div>
+    <status-message v-if="couponsData.length === 0">{{'建立新的優惠券'}}</status-message>
+    <table class="table mt-4 align-middle" v-else>
+      <thead>
+        <tr>
+          <th scope="col">優惠券名稱</th>
+          <th scope="col">折扣</th>
+          <th scope="col">到期日</th>
+          <th scope="col">代碼</th>
+          <th scope="col">是否啟用</th>
+          <th scope="col">操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in couponsData" :key="item.id">
+          <th>{{ item.title }}</th>
+          <td>{{ item.percent }}</td>
+          <td>{{ timestamp10CodeToDay(item.due_date) }}</td>
+          <td>{{ item.code }}</td>
+          <td>
+            <span v-if="item.is_enabled === 1" class="text-main-spec fw-bold">啟用</span>
+            <span v-else class="text-deep-gray">未啟用</span>
+          </td>
+          <td>
+            <button type="button" class="btn btn-sm btn-normal-medium block w-50"
+              @click="getAdminDeleCouponModal(index)">刪除</button>
+            <button type="button" class="btn btn-sm btn-normal-medium block w-50"
+              @click="getAdminCouponModal(index)">編輯</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
+  <!-- 優惠券modal -->
+  <CouponModal 
+  ref="couponModal" 
+  :in-edit-coupon-mode="inEditCouponMode"
+  :show-data="showData"
+  @post-admin-coupon="postAdminCoupon"
+  @put-admin-coupon="putAdminCoupon"></CouponModal>
+  <!-- 刪除產品Modal -->
+  <DeleteModal ref="deleteModal" :show-data="showData" @delete-function="deleteAdminCoupon"></DeleteModal>
+  <!-- 結果modal -->
+  <ResultModal ref="resultModal" :server-message="serverMessage"></ResultModal>
 </template>
 <script>
-  // const { VITE_BASE_URL, VITE_API_PATH } = import.meta.env;
+  // pinia
+  import { mapState, mapActions } from 'pinia';
+  import timeStore from '@/stores/timeStore';
+  import adminStore from '@/stores/adminStore';
+  // components
+  import StatusMessage from '@/components/backend/StatusMessage.vue';
+  import ResultModal from '@/components/ResultModal.vue';
+  import DeleteModal from '@/components/backend/DeleteModal.vue';
+  import CouponModal from '@/components/backend/CouponModal.vue';
+  
+  const { VITE_BASE_URL, VITE_API_PATH } = import.meta.env;
 
+  export default {
+    data() {
+      return {
+        getRemoteData: false,
+        couponsData: [],
+        showData: {},
+        inEditCouponMode: true,
+        serverMessage: {
+          message: '',
+          success: true,
+        },
+      }
+    },
+    components: {
+      StatusMessage,
+      ResultModal,
+      DeleteModal,
+      CouponModal,
+    },
+    methods: {
+      // modal, 打開新增優惠券modal
+      getAdminAddCouponModal(){
+        this.inEditCouponMode = false;
+        this.showData = {};
+        this.$refs.couponModal.openModal();
+      },
+      // modal, 打開編輯優惠券modal
+      getAdminCouponModal(index){
+        this.inEditCouponMode = true;
+        this.showData = JSON.parse(JSON.stringify(this.couponsData[index]));
+        this.showData.due_date = this.timestamp10CodeToDay(this.showData.due_date);
+        this.$refs.couponModal.openModal();
+      },
+      // modal, 打開刪除優惠券modal
+      getAdminDeleCouponModal(index){
+        this.showData = JSON.parse(JSON.stringify(this.couponsData[index]));
+        this.$refs.deleteModal.openModal();
+      },
+      getAdminCoupons(){
+        const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/coupons`;
+        this.$http.get(url)
+        .then(res=>{
+          this.getRemoteData = res.data.success;
+          this.couponsData = res.data.coupons;
+        })
+        .catch(err=>{
+          this.serverMessage.message = err.response.data.message;
+          this.serverMessage.success = err.response.data.success;
+          this.$refs.resultModal.openModal();
+        })
+      },
+      postAdminCoupon(updatedData){
+        const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/coupon`;
+        const data = {
+          data: updatedData,
+        };
+        data.data.due_date = this.dayToTimestamp10Code(updatedData.due_date);
+        this.$http.post(url, data)
+          .then(res=>{
+            this.getAdminCoupons();
+            this.serverMessage.message = res.data.message;
+            this.serverMessage.success = res.data.success;
+            this.$refs.couponModal.hideModal();
+            this.$refs.resultModal.openModal();
+          })
+          .catch(err=>{
+            this.serverMessage.message = err.response.data.message;
+            this.serverMessage.success = err.response.data.success;
+            this.$refs.resultModal.openModal();
+          })
+      },
+      putAdminCoupon(updatedData){
+        const id = updatedData.id;
+        const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/coupon/${id}`;
+        const data = {
+          data: updatedData,
+        };
+        data.data.due_date = this.dayToTimestamp10Code(updatedData.due_date);
+        this.$http.put(url, data)
+          .then(res=>{
+            this.getAdminCoupons();
+            this.serverMessage.message = res.data.message;
+            this.serverMessage.success = res.data.success;
+            this.$refs.couponModal.hideModal();
+            this.$refs.resultModal.openModal();
+          })
+          .catch(err=>{
+            this.serverMessage.message = err.response.data.message;
+            this.serverMessage.success = err.response.data.success;
+            this.$refs.resultModal.openModal();
+          });
+      },
+      deleteAdminCoupon(){
+        const id = this.showData.id;
+        const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/coupon/${id}`;
+        this.$http.delete(url)
+          .then(res=>{
+            this.getAdminCoupons();
+            this.serverMessage.message = res.data.message;
+            this.serverMessage.success = res.data.success;
+            this.$refs.deleteModal.hideModal();
+            this.$refs.resultModal.openModal();
+          })
+          .catch(err=>{
+            this.serverMessage.message = err.response.data.message;
+            this.serverMessage.success = err.response.data.success;
+            this.$refs.resultModal.openModal();
+          })
+      },
+      ...mapActions(adminStore, ['initializeAdmin', 'postApiUserCheck',]),
+      ...mapActions(timeStore, ['dayToTimestamp10Code', 'timestamp10CodeToDay',]),
+    },
+    computed: {
+      ...mapState(adminStore, ['loginSuccess',]),
+    },
+    mounted(){
+      if(this.loginSuccess){
+        this.getAdminCoupons();
+      }
+    },
+  }
 </script>
