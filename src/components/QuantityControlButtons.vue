e645ea726e1d4ebb0de89122654606896d9ad88b
YiJing0502 <w71080635@gmail.com>
2024-02-06 13:19:33 +0800

    feat:新增數量控制按鈕元件、新增結帳頁的內容

---
+++ src/components/QuantityControlButtons.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="d-flex">
+    <div class="bg-white d-flex w-50 mb-3 gap-3 border">
+      <button :disabled="currentNum===1" type="button" class="btn btn-lg" @click="minusNum">-</button>
+      <input type="number" class="form-control border-white shadow-none text-center" v-model.number="currentNum" @blur="blurNum">
+      <button :disabled="currentNum===inventory" type="button" class="btn btn-lg" @click="plusNum">+</button>
+    </div>
+    <p class="d-flex align-items-end ms-3">目前庫存：{{ inventory }}</p>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        currentNum: 1,
+        currentQty: 1,
+      }
+    },
+    props: ['inventory', 'id', 'qty','productCartId'],
+    emit: ['putNum'],
+    methods: {
+      // 按鈕, 增加數量
+      plusNum(){
+        this.currentNum = parseInt(this.currentNum);
+        if(this.currentNum >= 1 && this.currentNum<this.inventory){
+          this.currentNum += 1;
+          this.$emit('putNum', this.productCartId, this.id, this.currentNum);
+          return;
+        };
+        if(this.currentNum >= this.inventory){
+          alert('很抱歉，不能超出此庫存量');
+          return;
+        };
+      },
+      // 按鈕, 減少數量
+      minusNum(){
+        this.currentNum = parseInt(this.currentNum);
+        if(this.currentNum > 1){
+          this.currentNum -= 1;
+          this.$emit('putNum', this.productCartId, this.id, this.currentNum);
+          return;
+        };
+        if(this.currentNum <= 1){
+          alert('很抱歉，最低數量為1');
+          return;
+        };
+      },
+      // 輸入, 自訂數量
+      blurNum(e){
+        this.currentNum = parseInt(e.target.value);
+        if(this.currentNum > this.inventory){
+          this.currentNum = this.inventory;
+          this.$emit('putNum', this.productCartId, this.id, this.currentNum);
+          alert('很抱歉，不能超出此庫存量');
+          return;
+        }else if(this.currentNum <= 0){
+          this.currentNum = 1;
+          this.$emit('putNum', this.productCartId, this.id, this.currentNum);
+          alert('很抱歉，最低數量為1');
+          return;
+        }else if(this.currentNum === this.currentQty){
+          return;
+        }else if(isNaN(this.currentNum)){
+          alert('請輸入有效的數字');
+          this.currentNum = this.currentQty;
+          return;
+        };
+        this.$emit('putNum', this.productCartId, this.id, this.currentNum);
+      }
+    },
+    watch: {
+      qty(newQty){
+        this.currentNum = newQty;
+        this.currentQty = newQty;
+      }
+    },
+    mounted() {
+      if(this.qty > this.inventory){
+        this.$emit('putNum', this.productCartId, this.id, this.inventory);
+      };
+      this.currentNum = this.qty;
+      this.currentQty = this.qty;
+    },
+  }; 
+</script>
--- src/views/frontend/CheckoutView.vue
+++ src/views/frontend/CheckoutView.vue
@@ -4,7 +4,26 @@
   </div>
 </template>
 <script>
+  import { mapActions, mapState } from 'pinia';
+  import ordersStore from '@/stores/ordersStore';
+  import cartsStore from '@/stores/cartsStore';
+  // component
+  import QuantityControlButtons from '@/components/QuantityControlButtons.vue';
+  
   export default {
     
   };
 </script>
+<style>
+  /* Chrome, Safari, Edge, Opera */
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+
+  /* Firefox */
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
+</style>
