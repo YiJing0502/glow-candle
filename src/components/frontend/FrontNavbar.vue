517da69d53aa4e3ad95ce1c0c802ce2814da1bf1
YiJing0502 <w71080635@gmail.com>
2024-02-06 09:55:00 +0800

    feat:新增基本的前台導覽列

---
+++ src/components/frontend/FrontNavbar.vue
@@ -0,0 +1,71 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <div class="container">
+      <router-link :to="{name: 'front'}" class="navbar-brand" ><img src="../../../public/glow-logo.png" alt="glow-logo" width="50"></router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link :to="{name: 'front'}" class="nav-link" aria-current="page">首頁</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              產品類別
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              從系列選擇
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              從調性選擇
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name: 'front'}" class="nav-link" aria-current="page">關於GLOW</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name: 'front'}" class="nav-link" aria-current="page">訊息</router-link>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              搜尋
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">Action</a></li>
+              <li><a class="dropdown-item" href="#">Another action</a></li>
+              <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <router-link :to="{name: 'front'}" class="nav-link" aria-current="page">購物車</router-link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+<script>
+  export default {
+    
+  }
+</script>
--- src/views/frontend/FrontLayoutView.vue
+++ src/views/frontend/FrontLayoutView.vue
@@ -1,12 +1,17 @@
 <template>
+  <FrontNavbar></FrontNavbar>
   <router-view :key="$route.fullPath"></router-view>
 </template>
 <script>
+  import FrontNavbar from '@/components/frontend/FrontNavbar.vue';
   export default {
     data(){
       return {
         path: '/',
       }
+    },
+    components: {
+      FrontNavbar,
     },
     watch: {
       $route() {
