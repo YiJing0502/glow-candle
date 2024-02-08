e1dd3b27b63665b70478b5631c5602a3e8f495dc
YiJing0502 <w71080635@gmail.com>
2024-02-08 14:20:21 +0800

    fix:新增返回購物按鈕在購物車為空時

--- src/views/frontend/CheckoutView.vue
+++ src/views/frontend/CheckoutView.vue
@@ -30,7 +30,10 @@
               aria-labelledby="ShoppingCartSection">
               <div class="accordion-body">
                 <div class="" v-if="cartsData.length === 0">
-                  目前購物車內沒有商品
+                  <p>目前購物車內沒有商品 😄</p>
+                  <router-link :to="{name: 'products'}" class="btn btn-solid-spec w-100 btn-lg">
+                    繼續購物
+                  </router-link>
                 </div>
                 <div v-else class="">
                   <div class="row mb-3 position-relative" v-for="item in cartsData" :key="item.id">
