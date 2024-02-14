cfb6b15716feebbba087f3e276e3e58f91f9dd1a
YiJing0502 <w71080635@gmail.com>
2024-02-14 09:27:50 +0800

    fix:在 vite.config.js 加入 base

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,10 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/glow-candle/',
   plugins: [
     vue(),
   ],
-  base: '/glow-candle',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
