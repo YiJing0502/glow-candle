45c3dd93dbbe0e3cabea2b5cf1314c973d5b3b97
YiJing0502 <w71080635@gmail.com>
2024-02-14 14:45:29 +0800

    fix:修正錯誤(eslint)

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="d-flex flex-column justify-content-center
-     align-items-center position-absolute top-50 start-50 translate-middle"
+    class="d-flex flex-column justify-content-center align-items-center position-absolute
+     top-50 start-50 translate-middle"
   >
     <h2 class="fs-1">404</h2>
     <h3 class="fs-2">很抱歉，找不到此頁面</h3>
