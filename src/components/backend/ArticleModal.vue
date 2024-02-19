a76a838dcac47a8269fb48351abb4e192d56b784
YiJing0502 <w71080635@gmail.com>
2024-02-19 16:52:12 +0800

    feat: 新增後台文章頁面、文章Modal（新增文章ok）

---
+++ src/components/backend/ArticleModal.vue
@@ -0,0 +1,321 @@
+<template>
+  <!-- Modal -->
+  <div
+    ref="modal"
+    class="modal fade"
+    data-bs-backdrop="static"
+    data-bs-keyboard="false"
+    tabindex="-1"
+    aria-labelledby="staticBackdropLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-xl">
+      <div class="modal-content">
+        <vee-form
+          @submit="inEditCouponMode ? putAdminArticle() : postAdminArticle()"
+          v-slot="{ errors }"
+        >
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="staticBackdropLabel">
+              {{ inEditArticleMode ? '編輯文章' : '新增文章' }}
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <TwoTabs :tabs="tabs">
+              <template v-slot:0>
+                <!-- 標題、公開 -->
+                <div class="row mb-3">
+                  <div class="col-10">
+                    <label for="author" class="form-label"
+                      >標題
+                      <span class="text-main-spec fw-bold">*</span>
+                    </label>
+                    <vee-field
+                      type="text"
+                      name="標題"
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['標題'] }"
+                      rules="required|max:100"
+                      id="author"
+                      placeholder="請輸入標題"
+                      v-model="updatedShowData.title"
+                    />
+                    <vee-error-message class="invalid-feedback" name="標題"></vee-error-message>
+                  </div>
+                  <div class="col-2 d-flex align-items-center">
+                    <div class="form-check mt-4">
+                      <input
+                        type="checkbox"
+                        class="form-check-input"
+                        id="isPublic"
+                        v-model="updatedShowData.isPublic"
+                      />
+                      <label for="isPublic" class="form-check-label">
+                        <span v-if="updatedShowData.isPublic" class="text-main-spec fw-bold"
+                          >公開</span
+                        >
+                        <span v-else class="text-deep-gray">不公開</span>
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <!-- 標籤 -->
+                <div class="">
+                  <div class="row row-cols-4">
+                    <div class="col"
+                      v-for="(item, index) in updatedShowData.tag"
+                      :key="index">
+                      <label for="tag" class="form-label"
+                        >標籤{{ index }}
+                        <span class="text-main-spec fw-bold">*</span>
+                      </label>
+                      <div
+                        class="col mb-3"
+                      >
+                        <div class="input-group input-group-sm">
+                          <vee-field
+                            type="text"
+                            :name="'標籤' + index"
+                            class="form-control"
+                            :class="{ 'is-invalid': errors['標籤' + index] }"
+                            rules="required|max:100"
+                            id="tag"
+                            v-model="updatedShowData.tag[index]"
+                            placeholder="標籤名稱"
+                          />
+                          <button
+                            type="button"
+                            class="btn btn-normal-spec"
+                            @click="updatedShowData.tag.splice(inex, 1)"
+                            v-if="index !== 0"
+                          >
+                            x
+                          </button>
+                          <vee-error-message
+                            class="invalid-feedback"
+                            :name="'標籤' + index"
+                          ></vee-error-message>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <button
+                      type="button"
+                      class="btn btn-outline-mdgray"
+                      @click="updatedShowData.tag.push('')"
+                    >
+                      新增標籤
+                    </button>
+                  </div>
+                </div>
+                <!-- 作者名稱、創立日期 -->
+                <div class="row mb-3">
+                  <div class="col">
+                    <label for="author" class="form-label"
+                      >作者名稱
+                      <span class="text-main-spec fw-bold">*</span>
+                    </label>
+                    <vee-field
+                      type="text"
+                      name="作者名稱"
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['作者名稱'] }"
+                      rules="required|max:100"
+                      id="author"
+                      placeholder="請輸入作者名稱"
+                      v-model="updatedShowData.author"
+                    />
+                    <vee-error-message
+                      class="invalid-feedback"
+                      name="作者名稱"
+                    ></vee-error-message>
+                  </div>
+                  <div class="col">
+                    <label for="create_at" class="form-label"
+                      >創立日期
+                      <span class="text-main-spec fw-bold">*</span>
+                    </label>
+                    <vee-field
+                      type="date"
+                      name="創立日期"
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['創立日期'] }"
+                      rules="required"
+                      id="create_at"
+                      placeholder="請輸入創立日期"
+                      v-model="updatedShowData.create_at"
+                      :min="currentDate"
+                    />
+                    <vee-error-message
+                      class="invalid-feedback"
+                      name="創立日期"
+                    ></vee-error-message>
+                  </div>
+                </div>
+                <!-- 描述、圖片 -->
+                <div class="row mb-3">
+                  <div class="col">
+                    <label for="description" class="form-label">描述
+                      <span class="text-main-spec fw-bold">*</span>
+                    </label>
+                    <vee-field
+                      as="textarea"
+                      name="描述"
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['描述'] }"
+                      rules="max:800|required"
+                      id="description"
+                      rows="3"
+                      v-model="updatedShowData.description"
+                    ></vee-field>
+                    <vee-error-message class="invalid-feedback" name="描述"></vee-error-message>
+                  </div>
+                  <div class="col-4">
+                    <label for="imageUrl" class="form-label">輸入主要圖片網址
+                      <span class="text-main-spec fw-bold">*</span>
+                    </label>
+                    <vee-field
+                      type="text"
+                      name="主要圖片網址"
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['主要圖片網址'] }"
+                      rules="url|required"
+                      placeholder="請輸入主要圖片連結"
+                      v-model="updatedShowData.imageUrl"
+                    />
+                    <vee-error-message
+                      class="invalid-feedback"
+                      name="主要圖片網址"
+                    ></vee-error-message>
+                    <img
+                      :src="updatedShowData.imageUrl"
+                      :alt="updatedShowData.title"
+                      class="img-fluid"
+                    />
+                  </div>
+                  <!-- 上傳單圖 -->
+                  <div class="col-4">
+                    <label for="formFile" class="form-label">上傳主要圖片檔案</label>
+                    <vee-field
+                      class="form-control"
+                      :class="{ 'is-invalid': errors['圖片上傳'] }"
+                      type="file"
+                      name="圖片上傳"
+                      rules="size:3072|ext:jpeg,jpg,png"
+                      @change="goToUploadImage"
+                      ref="uploadInput"
+                      accept="image/jpeg, image/jpg, image/png"
+                    />
+                    <vee-error-message
+                      class="invalid-feedback"
+                      name="圖片上傳"
+                    ></vee-error-message>
+                    <ul>
+                      <li>限jpg、jpeg、png類型的檔案</li>
+                      <li>小於3MB</li>
+                      <li>單圖上傳</li>
+                    </ul>
+                  </div>
+                </div>
+              </template>
+              <template v-slot:1>
+                <ckeditor
+                :editor="editor"
+                v-model="updatedShowData.content"
+                :config="editorConfig"></ckeditor>
+              </template>
+            </TwoTabs>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-normal-dpgray" data-bs-dismiss="modal">
+              關閉
+            </button>
+            <button type="submit" class="btn btn-solid-spec">
+              {{ inEditArticleMode ? '更新' : '新增' }}
+            </button>
+          </div>
+        </vee-form>
+      </div>
+    </div>
+  </div>
+  <ResultModal ref="resultModal" :server-message="serverMessage"></ResultModal>
+</template>
+<script>
+import { Modal } from 'bootstrap';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { mapState, mapActions } from 'pinia';
+import timeStore from '../../stores/timeStore';
+import uploadImagesStore from '../../stores/uploadImagesStore';
+import TwoTabs from './TwoTabs.vue';
+
+export default {
+  data() {
+    return {
+      updatedShowData: {},
+      serverMessage: {
+        message: '',
+        success: true,
+      },
+      tabs: [{ label: '文章資料' }, { label: '文章內容' }],
+      // editor
+      editor: ClassicEditor,
+      editorConfig: {
+        // The configuration of the editor.
+      },
+    };
+  },
+  props: ['inEditArticleMode', 'showData'],
+  emits: ['post-admin-article'],
+  watch: {
+    showData() {
+      this.updatedShowData = JSON.parse(JSON.stringify(this.showData));
+    },
+  },
+  computed: {
+    ...mapState(timeStore, ['currentDate']),
+  },
+  components: {
+    TwoTabs,
+  },
+  methods: {
+    openModal() {
+      this.modal.show();
+    },
+    hideModal() {
+      this.modal.hide();
+    },
+    putAdminArticle() {},
+    postAdminArticle() {
+      this.$emit('post-admin-article', this.updatedShowData);
+    },
+    async goToUploadImage(event) {
+      try {
+        const res = await this.uploadImages(event);
+        this.handleServerResponse(true, '上傳成功', res.data.imageUrl);
+      } catch (err) {
+        this.handleServerResponse(false, err.response.data.message);
+      }
+    },
+    handleServerResponse(success, message, imageUrl) {
+      this.serverMessage.message = message;
+      this.serverMessage.success = success;
+      this.$refs.resultModal.openModal();
+      this.$refs.uploadInput.value = '';
+      if (success) {
+        this.updatedShowData.imageUrl = imageUrl;
+      }
+    },
+    ...mapActions(uploadImagesStore, ['uploadImages']),
+  },
+  mounted() {
+    this.modal = new Modal(this.$refs.modal);
+  },
+};
+</script>
---
+++ src/views/backend/AdminArticlesView.vue
@@ -0,0 +1,200 @@
+<template>
+  <VueLoading
+    v-if="!getRemoteData"
+    :active="!getRemoteData"
+    :background-color="'#FBFAF4'"
+    :color="'#52504B'"
+  />
+  <div v-else class="container">
+    <div class="row mt-4">
+      <div class="col d-flex justify-content-end">
+        <button type="button" class="btn btn-solid-spec" @click="getAdminAddArticleModal">
+          建立新的文章
+        </button>
+      </div>
+    </div>
+    <status-message v-if="articlesData.length === 0">{{
+      '請點擊「建立新的文章」新增資料'
+    }}</status-message>
+    <table class="table mt-4 align-middle" v-else>
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">圖片</th>
+          <th scope="col">標題</th>
+          <th scope="col">標籤</th>
+          <th scope="col">創立日期</th>
+          <th scope="col">作者</th>
+          <th scope="col">描述</th>
+          <th scope="col">狀態</th>
+          <th scope="col">操作</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, index) in articlesData" :key="item.id">
+          <th>{{ item.num }}</th>
+          <th>
+            <img :src="item.imageUrl" :alt="item.title" class="img-fluid" width="50" />
+          </th>
+          <td>{{ item.title }}</td>
+          <td>
+            <span
+            class="badge bg-medium-gray text-deep-gray me-1"
+            v-for="(tag, index) in item.tag"
+            :key="index"
+            >{{ tag }}</span>
+          </td>
+          <td>{{ timestamp10CodeToDay(item.create_at) }}</td>
+          <td>{{ item.author }}</td>
+          <td>{{ item.description }}</td>
+          <td>
+            <span v-if="item.isPublic" class="text-main-spec fw-bold">公開</span>
+            <span v-else class="text-deep-gray">非公開</span>
+          </td>
+          <td>
+            <button
+              type="button"
+              class="btn btn-sm btn-normal-medium block w-50"
+              @click="getAdminDeleArticleModal(index)"
+            >
+              刪除
+            </button>
+            <button
+              type="button"
+              class="btn btn-sm btn-normal-medium block w-50"
+              @click="getAdminArticleModal(index)"
+            >
+              編輯
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+    <div v-if="pagination && pagination.current_page" class="d-flex justify-content-center mt-4">
+      <nav aria-label="Page navigation example">
+        <ul class="pagination" id="pageid">
+          <PageBtn
+            :prev-is-enabled="pagination.has_pre"
+            :next-is-enabled="pagination.has_next"
+            :totalPage="pagination.total_pages"
+            :current-page="pagination.current_page"
+            @change-prev-page="getAdminCoupons(pagination.current_page - 1)"
+            @change-next-page="getAdminCoupons(pagination.current_page + 1)"
+            @change-page="goToChangePage"
+          ></PageBtn>
+        </ul>
+      </nav>
+    </div>
+  </div>
+  <ArticleModal
+    ref="articleModal"
+    :in-edit-article-mode="inEditArticleMode"
+    :show-data="showData"
+    @post-admin-article="postAdminArticle"
+  ></ArticleModal>
+  <!-- 刪除產品Modal -->
+  <!-- <DeleteModal
+    ref="deleteModal"
+    :show-data="showData"
+    @delete-function="deleteAdminArticle"
+  ></DeleteModal> -->
+  <!-- 結果modal -->
+  <ResultModal ref="resultModal" :server-message="serverMessage"></ResultModal>
+</template>
+<script>
+import PageBtn from '../../components/PageBtn.vue';
+import StatusMessage from '../../components/backend/StatusMessage.vue';
+import DeleteModal from '../../components/backend/DeleteModal.vue';
+import ArticleModal from '../../components/backend/ArticleModal.vue';
+import timeStore from '../../stores/timeStore';
+import { mapState, mapActions } from 'pinia';
+
+const { VITE_BASE_URL, VITE_API_PATH } = import.meta.env;
+export default {
+  data() {
+    return {
+      getRemoteData: true,
+      articlesData: [],
+      pagination: {},
+      showData: {},
+      inEditArticleMode: true,
+      serverMessage: {
+        message: '',
+        success: true,
+      },
+    };
+  },
+  components: {
+    PageBtn,
+    StatusMessage,
+    DeleteModal,
+    ArticleModal,
+  },
+  computed: {
+    ...mapState(timeStore, ['currentDate']),
+  },
+  methods: {
+    // modal, 打開新增文章modal
+    getAdminAddArticleModal() {
+      this.inEditArticleMode = false;
+      this.showData = {
+        tag: [''],
+        content: '<p>輸入文章內容...</p>',
+        create_at: this.currentDate,
+        isPublic: false,
+      };
+      this.$refs.articleModal.openModal();
+    },
+    // // modal, 打開編輯文章modal
+    // getAdminArticleModal(index){},
+    // // modal, 打開刪除文章modal
+    // getAdminDeleArticleModal(index){},
+    // // fn, 切換頁數
+    // goToChangePage(page) {
+    //   this.getAdminArticles(page);
+    // },
+    // ajax, 取得 articles
+    getAdminArticles(page = 1) {
+      const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/articles`;
+      this.$http
+        .get(url, { params: { page } })
+        .then((res) => {
+          this.articlesData = res.data.articles;
+          console.log(this.articlesData);
+        })
+        .catch((err) => {
+          this.handleServerResponse(false, err.response.data.message);
+        });
+    },
+    // ajax, 新增 articles
+    postAdminArticle(updatedData) {
+      const url = `${VITE_BASE_URL}/v2/api/${VITE_API_PATH}/admin/article`;
+      const data = { data: updatedData };
+      data.data.create_at = this.dayToTimestamp10Code(updatedData.create_at);
+      this.$http
+        .post(url, data)
+        .then((res) => {
+          this.$refs.articleModal.hideModal();
+          this.handleServerResponse(true, res.data.message);
+        })
+        .catch((err) => {
+          this.handleServerResponse(false, err.response.data.message);
+        });
+    },
+    // // ajax, 修改 articles
+    // putAdminArticle(updatedData){},
+    // // ajax, 刪除 articles
+    // deleteAdminArticle(){},
+    // 處理 伺服器 訊息
+    handleServerResponse(success, message) {
+      this.serverMessage.message = message;
+      this.serverMessage.success = success;
+      this.$refs.resultModal.openModal();
+    },
+    ...mapActions(timeStore, ['dayToTimestamp10Code', 'timestamp10CodeToDay']),
+  },
+  mounted() {
+    this.getAdminArticles();
+  },
+};
+</script>
