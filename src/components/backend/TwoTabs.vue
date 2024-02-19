608b549e2ee7205e6ad2ebb1acc9bca738dd0e1d
YiJing0502 <w71080635@gmail.com>
2024-02-19 16:50:40 +0800

    feat: 新增 tab 元件（bootstrap）

---
+++ src/components/backend/TwoTabs.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav>
+    <div class="nav nav-tabs" id="nav-tab" role="tablist">
+      <button
+        class="nav-link active w-50"
+        id="nav-home-tab"
+        data-bs-toggle="tab"
+        data-bs-target="#nav-home"
+        type="button"
+        role="tab"
+        aria-controls="nav-home"
+        aria-selected="true"
+      >
+        {{  tabs[0].label }}
+      </button>
+      <button
+        class="nav-link w-50"
+        id="nav-profile-tab"
+        data-bs-toggle="tab"
+        data-bs-target="#nav-profile"
+        type="button"
+        role="tab"
+        aria-controls="nav-profile"
+        aria-selected="false"
+      >
+      {{  tabs[1].label }}
+      </button>
+    </div>
+  </nav>
+  <div class="tab-content" id="nav-tabContent">
+    <div
+      class="tab-pane fade show active"
+      id="nav-home"
+      role="tabpanel"
+      aria-labelledby="nav-home-tab"
+      tabindex="0"
+    >
+      <slot name="0"></slot>
+    </div>
+    <div
+      class="tab-pane fade"
+      id="nav-profile"
+      role="tabpanel"
+      aria-labelledby="nav-profile-tab"
+      tabindex="0"
+    >
+      <slot name="1"></slot>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+    };
+  },
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+  },
+};
+</script>
