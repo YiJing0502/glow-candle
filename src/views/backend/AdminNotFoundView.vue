6f0cc3435dc3f4eb5f7d6be10e916be25571a5ba
YiJing0502 <w71080635@gmail.com>
2024-02-23 10:08:09 +0800

    fix: 修改後台 404 頁面的 顯示

--- src/views/backend/AdminNotFoundView.vue
+++ src/views/backend/AdminNotFoundView.vue
@@ -1,8 +1,6 @@
 <!-- eslint-disable max-len -->
 <template>
-  <div
-    class="d-flex flex-column justify-content-center align-items-center position-absolute top-50 start-50 translate-middle"
-  >
+  <div class="d-flex flex-column justify-content-center align-items-center vh-100 vw-100">
     <h2 class="fs-1">404</h2>
     <h3 class="fs-2">很抱歉，找不到此頁面</h3>
     <button class="btn btn-outline-dpgray fs-5 mt-3" @click="this.$router.go(-1)">
