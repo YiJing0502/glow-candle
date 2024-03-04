37e0929417a8cf6ef443c17d18f60d58f82e3d21
YiJing0502 <w71080635@gmail.com>
2024-03-04 11:49:30 +0800

    fix: NotFoundView 修改樣式

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,11 +1,11 @@
 <!-- eslint-disable max-len -->
 <template>
   <div
-    class="d-flex flex-column justify-content-center align-items-center position-absolute top-50 start-50 translate-middle"
+    class="d-flex flex-column justify-content-center align-items-center py-5 bg-main-medium container-rounded container my-5"
   >
-    <h2 class="fs-1">404</h2>
-    <h3 class="fs-2">很抱歉，找不到此頁面</h3>
-    <button class="btn btn-outline-dpgray fs-5 mt-3" @click="this.$router.go(-1)">
+    <span class="fs-1 material-icons mb-3">error</span>
+    <h3 class="fs-2">找不到此頁面</h3>
+    <button class="btn btn-outline-dpgray fs-5 mt-3" @click="this.$router.go(-2)">
       返回上一頁
     </button>
   </div>
